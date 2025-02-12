7ec8f1dc0ea732801d73813fde24c30865152cd6
marieliburi silva <marieliburi10@gmail.com>
2025-02-12 17:03:43 +0000

    Primeiro commit - salvando classe pessoa

---
+++ Pessoa.java
@@ -0,0 +1,22 @@
+public class Pessoa{
+    private String nome = "Marieli";
+    private int idade = 18;
+
+    //Get
+    public int getIdade() {
+        return idade;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    //Set
+    public void setNome(String nome) {
+            this.nome = nome;
+        }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
+}
